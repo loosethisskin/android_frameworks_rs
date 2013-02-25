c6d990aa2ec18beabc1ac7ce5f58f6103c61e750
Paul Beeler <sparksco@gmail.com>
2013-02-25 16:36:55 -0700

    Clang: Silence warnings (3/3)
    With GCC >=4.7 Clang generates a lot of noise with various compiler flags,
    and can even break the builds when using Flags like Graphite.  This can be
    reproduced by doing the following:
    
    Setting TARGET_EXTRA_CFLAGS with Graphite flags such as these
    TARGET_EXTRA_CFLAGS +=  -fgraphite-identity \
                            -floop-block \
                            -floop-strip-mine \
                            -ftree-loop-distribution \
                            -ftree-loop-linear
    
    Causes the following errors to occure and the builds to fail:
    
    clang: error: argument unused during compilation: '-fgraphite-identity'
                                                      '-floop-block'
                                                      '-floop-strip-mine'
                                                      '-ftree-loop-distribution'
                                                      '-ftree-loop-linear'
    
    Silence the errors by passing the -Qunused-arguments for CFLAGS
    
    Singed-off-by: Paul Beeler <sparksco@gmail.com>
    
    More clang fixes
    
    fix Qunused-arguments
    Signed-off-by: Paul Beeler <sparksco@gmail.com>
    
    Use -Qunused-arguments for LibRs
    Signed-off-by: Paul Beeler <sparksco@gmail.com>
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -57,7 +57,7 @@ LOCAL_SHARED_LIBRARIES += libbcc libbcinfo libui libgui libsync
 
 LOCAL_C_INCLUDES += frameworks/compile/libbcc/include
 
-LOCAL_CFLAGS += $(rs_base_CFLAGS)
+LOCAL_CFLAGS += $(rs_base_CFLAGS) -Qunused-arguments
 
 LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE_TAGS := optional
@@ -171,7 +171,7 @@ LOCAL_STATIC_LIBRARIES := libft2
 LOCAL_C_INCLUDES += external/freetype/include
 LOCAL_C_INCLUDES += frameworks/compile/libbcc/include
 
-LOCAL_CFLAGS += $(rs_base_CFLAGS)
+LOCAL_CFLAGS += $(rs_base_CFLAGS) -Qunused-arguments
 
 LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE_TAGS := optional
@@ -218,6 +218,7 @@ LOCAL_GENERATED_SOURCES += $(GEN)
 LOCAL_CFLAGS += $(rs_base_CFLAGS)
 LOCAL_CFLAGS += -DANDROID_RS_SERIALIZE
 LOCAL_CFLAGS += -fPIC
+LOCAL_CFLAGS += -Qunused-arguments
 
 LOCAL_SRC_FILES:= \
 	rsAdapter.cpp \
