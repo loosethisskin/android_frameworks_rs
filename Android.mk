0bad2a37d70a812e9829ace49a5772c4018dfaa3
Paul Beeler <sparksco@gmail.com>
2013-04-10 00:39:47 -0600

    Add -Wno-unknown-warning-option for libRSDriver
    Signed-off-by: Paul Beeler <sparksco@gmail.com>
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -55,7 +55,7 @@ LOCAL_SHARED_LIBRARIES += libbcc libbcinfo libui libgui libsync
 
 LOCAL_C_INCLUDES += frameworks/compile/libbcc/include
 
-LOCAL_CFLAGS += $(rs_base_CFLAGS) -Qunused-arguments
+LOCAL_CFLAGS += $(rs_base_CFLAGS) -Qunused-arguments -Wno-unknown-warning-option
 
 LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE_TAGS := optional
